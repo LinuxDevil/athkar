f0d93f62d8162d4298b28b15b401e4c855c08040
Ali Mohammad <alimohammad@pc-2473.home>
2023-03-27 00:15:26 +0300

    feature: add athkar command to package.json

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ let interval: NodeJS.Timeout;
 export function activate(context: vscode.ExtensionContext) {
 
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('athkar.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('athkar.activate', () => {
 		interval = setInterval(showAthkar, 60000);
 		vscode.window.showInformationMessage('Athkar will show now every 1 hour!');
 	});
