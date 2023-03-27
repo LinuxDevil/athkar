771b2eabe27abef9ce0d437fe92be747442eb3b9
Ali Mohammad <alimohammad@pc-2473.home>
2023-03-27 17:54:32 +0300

    chore: Update the readme

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,6 @@ Runs Every 1 Hour
 
 - Initial release 0.02
 Update the usage section of the README.md file
+
+- Initial release 0.03
+Update the usage section of the README.md file
--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('athkar.activate', () => {
-		interval = setInterval(showAthkar, 60000);
+		interval = setInterval(showAthkar, 3600000);
 		vscode.window.showInformationMessage('Athkar will show now every 1 hour!');
 	});
 
