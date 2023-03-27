706d9d7f3c411936c650fd2fcc3a9b1efc4f4bfe
Ali Mohammad <alimohammad@pc-2473.home>
2023-03-27 18:38:26 +0300

    feature: add athkar deactivate command and configuration for the interval

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,8 @@ Update the usage section of the README.md file
 
 - Initial release 0.03
 Update the usage section of the README.md file
+
+- Initial release 0.04
+Updated the interval to 1 hour
+Added a new command to deactive the timer
+Added a new configuration option to set the interval
--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,23 @@
 import * as vscode from 'vscode';
 import { showAthkar } from './islamic-athkar';
 let interval: NodeJS.Timeout;
+const config = vscode.workspace.getConfiguration('athkar');
+const timeIntervalInMinutes = config.get('timeIntervalInMinutes', 60);
 
 export function activate(context: vscode.ExtensionContext) {
 
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('athkar.activate', () => {
-		interval = setInterval(showAthkar, 3600000);
+		interval = setInterval(showAthkar, timeIntervalInMinutes * 60000);
 		vscode.window.showInformationMessage('Athkar will show now every 1 hour!');
 	});
 
+	let deactivate = vscode.commands.registerCommand('athkar.deactivate', () => {
+		clearInterval(interval);
+		vscode.window.showInformationMessage('Athkar Is Deactivated!');
+	});
+
+	context.subscriptions.push(deactivate);
 	context.subscriptions.push(disposable);
 }
 
